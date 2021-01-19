e720f75c97b90561e749a34950adda2c44831641
nicogodet <nicolas.godet@outlook.fr>
2021-01-19 10:34:39 +0100

    Rename algs

--- scripts/txt2tif.py
+++ scripts/txt2tif.py
@@ -1,8 +1,8 @@
 """
 Model exported as python.
-Name : shp2tif
+Name : txt2asc
 Group : Post Traitement
-With QGIS : 31203
+With QGIS : 312
 """
 
 from qgis.core import (
@@ -10,7 +10,7 @@ from qgis.core import (
     QgsProcessingAlgorithm,
     QgsProcessingUtils,
     QgsProcessingMultiStepFeedback,
-    QgsProcessingParameterField,
+    QgsProcessingParameterFile,
     QgsProcessingParameterVectorLayer,
     QgsProcessingParameterNumber,
     QgsProcessingParameterFileDestination,
@@ -21,23 +21,14 @@ from qgis.core import (
 import processing
 
 
-class Shp2tif(QgsProcessingAlgorithm):
+class Txt2asc(QgsProcessingAlgorithm):
     def initAlgorithm(self, config=None):
         self.addParameter(
-            QgsProcessingParameterVectorLayer(
-                "couche",
-                "Couche de points à interpoler",
-                types=[QgsProcessing.TypeVectorPoint],
-                defaultValue=None,
-            )
-        )
-        self.addParameter(
-            QgsProcessingParameterField(
-                "z_field",
-                "Champ à interpoler",
-                type=QgsProcessingParameterField.Numeric,
-                parentLayerParameterName="couche",
-                allowMultiple=False,
+            QgsProcessingParameterFile(
+                "Fichiertexte",
+                "Fichier texte",
+                behavior=QgsProcessingParameterFile.File,
+                fileFilter="Fichier texte (*.txt)",
                 defaultValue=None,
             )
         )
@@ -46,7 +37,7 @@ class Shp2tif(QgsProcessingAlgorithm):
                 "resolutiondurasterdusortie",
                 "Résolution du raster du sortie",
                 type=QgsProcessingParameterNumber.Integer,
-                minValue=0.01,
+                minValue=0.1,
                 defaultValue=1,
             )
         )
@@ -77,42 +68,9 @@ class Shp2tif(QgsProcessingAlgorithm):
         results = {}
         outputs = {}
 
-        layer = QgsProcessingUtils.mapLayerFromString(
-            parameters["couche"], context=context
-        )
-
-        tempTXT = QgsProcessingUtils.generateTempFilename("txt_temp.txt")
         tempLAS = QgsProcessingUtils.generateTempFilename("las_temp.las")
         if parameters["Raster"] == "TEMPORARY_OUTPUT":
             parameters["Raster"] = QgsProcessingUtils.generateTempFilename("tif_temp.tif")
-        
-        output_file = open(tempTXT, "w")
-        
-        # Get the features and properly rewrite them as lines
-        for feat in layer.getFeatures():
-            featGeom = feat.geometry()
-            if featGeom.wkbType() in [1, 1001, 2001, 3001, -2147483647]:
-                output_file.write(
-                    "%s\t%s\t%s\n"
-                    % (
-                        str(featGeom.asPoint().x()),
-                        str(featGeom.asPoint().y()),
-                        str(feat[parameters["z_field"]]),
-                    )
-                )
-            elif featGeom.wkbType() in [4, 1004, 2004, 3004, -2147483644]:
-                output_file.write(
-                    "%s\t%s\t%s\n"
-                    % (
-                        str(featGeom.asMultiPoint()[0].x()),
-                        str(featGeom.asMultiPoint()[0].y()),
-                        str(feat[parameters["z_field"]]),
-                    )
-                )
-            else:
-                output_file.close()
-                return {}
-        output_file.close()
 
         # txt2las
         alg_params = {
@@ -120,7 +78,7 @@ class Shp2tif(QgsProcessingAlgorithm):
             "CPU64": True,
             "EPSG_CODE": 25832,
             "GUI": False,
-            "INPUT_GENERIC": tempTXT,
+            "INPUT_GENERIC": parameters["Fichiertexte"],
             "PARSE": "xyz",
             "PROJECTION": 0,
             "SCALE_FACTOR_XY": 0.01,
@@ -132,11 +90,7 @@ class Shp2tif(QgsProcessingAlgorithm):
             "OUTPUT_LASLAZ": tempLAS,
         }
         outputs["Txt2las"] = processing.run(
-            "LAStools:txt2las",
-            alg_params,
-            context=context,
-            feedback=feedback,
-            is_child_algorithm=True,
+            "LAStools:txt2las", alg_params, context=context, feedback=feedback, is_child_algorithm=True,
         )
 
         feedback.setCurrentStep(5)
@@ -158,11 +112,7 @@ class Shp2tif(QgsProcessingAlgorithm):
             "OUTPUT_RASTER": parameters["Raster"],
         }
         outputs["Las2dem"] = processing.run(
-            "LAStools:blast2dem",
-            alg_params,
-            context=context,
-            feedback=feedback,
-            is_child_algorithm=True,
+            "LAStools:las2dem", alg_params, context=context, feedback=feedback, is_child_algorithm=True,
         )
 
         feedback.setCurrentStep(6)
@@ -179,15 +129,15 @@ class Shp2tif(QgsProcessingAlgorithm):
         )
         QgsProject.instance().addMapLayer(rlayer)
         return results
-        
+
     def flags(self):
         return super().flags() | QgsProcessingAlgorithm.FlagNoThreading
 
     def name(self):
-        return "shp2tif"
+        return "txt2tif"
 
     def displayName(self):
-        return "shp2tif"
+        return "txt2tif"
 
     def group(self):
         return "Interpolation"
@@ -196,4 +146,4 @@ class Shp2tif(QgsProcessingAlgorithm):
         return "Interpolation"
 
     def createInstance(self):
-        return Shp2tif()
+        return Txt2asc()
